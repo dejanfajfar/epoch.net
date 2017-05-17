6bf669ba190c9bb99fc14c9afebdcdd4f1417d6d
Dejan Fajfar <Dejan@Dejans-MacBook-Pro.local>
2017-05-17 22:40:36 +0200

    Initial commit

---
+++ Epoch.net/Epoch.net.Tests/DateTimeExtentionsFixture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Epoch.net.Tests
+{
+    [TestFixture]
+    public class DateTimeExtentionsFixture
+    {
+        [Test]
+        public void ToUnixEpoch_After1970_Utc_DateTime()
+        {
+            var epoch = new DateTime(2015, 12, 12, 0, 0, 0, DateTimeKind.Utc).ToEpoch();
+
+            Assert.That(epoch, Is.EqualTo(1449878400));
+        }
+
+        [Test]
+        public void ToUnixEpoch_Before1970_Utc_DateTime()
+        {
+            var epoch = new DateTime(1960, 12, 12, 0, 0, 0, DateTimeKind.Utc).ToEpoch();
+
+            Assert.That(epoch, Is.EqualTo(-285724800));
+        }
+    }
+}
---
+++ Epoch.net/Epoch.net/DateTimeExtentions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Epoch.net
+{
+    public static class DateTimeExtentions
+    {
+        public static int ToEpoch(this DateTime dateTime)
+        {
+            var timeSinceDisco = TimeZoneInfo.ConvertTimeToUtc(dateTime.ToUniversalTime()) - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            return (int) timeSinceDisco.TotalSeconds;
+        }
+    }
+}
